7a86dfc9323d172fb108ef6a4e01b79fb8b4a856
Edu <edu488@gmail.com>
2024-03-14 17:46:16 -0500

    01

---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,13 @@
+function saludar(){
+console.log('Hola Mundo');
+
+
+}saludar()
+
+function suma(){
+
+      return 2 + 2;  
+}suma()
+
+let resultado = suma();
+console.log(resultado);
---
+++ 02-operadores/01-artimeticos.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'Suma');//suma
+// console.log(a - b, 'Resta');//resta
+// console.log(a * b, 'multiplicaion');//multiplicaion
+// console.log(a / b, 'Division');//division
+// console.log(a % b, 'Modulo');//modulo
+// console.log(a ** b, 'Potencias');//pontecia
+
+//potencia
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+//decreciente
+console.log(--a);
+console.log(a--);
+console.log(a);
