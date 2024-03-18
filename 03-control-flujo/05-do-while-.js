acc91e0476426fc4d90a4bf70fedfa291492c723
Edu <edu488@gmail.com>
2024-03-17 19:18:01 -0500

    ,

--- 03-control-flujo/05-do-while-.js
+++ 03-control-flujo/05-do-while-.js
@@ -1,14 +1,14 @@
-// let i = 2;
-// while (i < 2){
-//     if(i % 2 == 0){
-//         console.log('numero par', i);
-
-//     }
-//     i++;
-// } 
-do {
+let i = 2;
+while (i < 2){
     if(i % 2 == 0){
         console.log('numero par', i);
-      }
-        i++;
-     }while(i<2);
+
+    }
+    i++;
+} 
+// do {
+//     if(i % 2 == 0){
+//         console.log('numero par', i);
+//       }
+//         i++;
+//      }while(i<2);
