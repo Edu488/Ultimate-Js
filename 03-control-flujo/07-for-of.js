6c8bc2aa2f1aa50b29fa8a1c143e80b3e3b483b1
Edu <edu488@gmail.com>
2024-03-18 18:05:23 -0500

    .

--- 03-control-flujo/07-for-of.js
+++ 03-control-flujo/07-for-of.js
@@ -1,12 +1,18 @@
 let animales = ['chancho', 'perro', 'gato', 'lobo', 'garza'];
 // let nombres = ['Toddy', 'oreo', 'Luna', 'Nala', 'Hera'];
 
-for (let animal of animales){
+// for (let animal of animales){
 
-    console.log(animal);
+//     console.log(animal);
 
-}
+// }
 // for (let apodos of nombres){
 
 //     console.log(nombres);
 // }
+let i = 0;
+
+while (i < animales.length){
+    console.log(animales[i]);
+    i ++;
+}
---
+++ 03-control-flujo/08-for-in.js
@@ -0,0 +1,17 @@
+let user ={
+    Id: 0000001,
+    name: 'Eduardo',
+    age: 25, 
+};
+for ( let prop in user) {
+
+    console.log(prop,':', user[prop]);
+
+}
+let animales=['chanchito feliz', 'lobo', 'Dragon'];
+
+for(let indice in animales){
+
+    console.log(indice, animales[indice]);
+
+}
