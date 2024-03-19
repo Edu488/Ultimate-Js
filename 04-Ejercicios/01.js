671d44735034a1cb5bad0ee9176a7f706b170b32
Edu <edu488@gmail.com>
2024-03-19 11:27:24 -0500

    .

--- 04-Ejercicios/01.js
+++ 04-Ejercicios/01.js
@@ -1,6 +1,12 @@
-function cualEsMayor(a, b){
+function cualEsMayor(a, b) {
+    //   if (a > b){
+    //    return(a);
+    //    }else{
+    //     return(b);
+    //}
+    return  (a > b) ? a : b;
+       
       
-
 }
 let mayor = cualEsMayor(10, 5);
 
