d577cae0ffb6958b726b3128a15093838eee0b41
Edu <edu488@gmail.com>
2024-03-19 17:27:12 -0500

    .

---
+++ 04-Ejercicios/03.js
@@ -0,0 +1,14 @@
+//indice que el valor no sea menor q cero y q exita en el array
+
+
+function getbyIdx(arr, idx){
+//     if (idx < 0  || arr.length <= idx){
+
+//         return 'elemento no existe';
+//     }
+//     return arr[idx];
+// }
+
+ let resultado =  getbyIdx ([1, 2], 2)
+
+ console.log(resultado);
