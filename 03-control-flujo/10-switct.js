13bf84c5298bf27749c0849dc15fdaae9bb9d95c
Edu <edu488@gmail.com>
2024-03-19 09:48:04 -0500

    .

---
+++ 03-control-flujo/10-switct.js
@@ -0,0 +1,15 @@
+let accion = 'salta';
+
+switch(accion){
+
+    case 'correr':
+    console.log('salio a', accion);
+     break;
+     case 'saltar':
+     console.log('salio a ', accion);
+     break;
+     default:
+        console.log('No salio');
+
+
+}
