e22a7727036c69d66f67529fd66797d8fee838f3
Edu <edu488@gmail.com>
2024-03-28 19:10:22 -0500

    .

--- 03-control-flujo/01-if.js
+++ 03-control-flujo/01-if.js
@@ -8,6 +8,7 @@ else if(edad > 13){
     console.log('puede ingresar con sus padre');
 
 }
-else {
-     console.log('no puede entar ');
-}
+
+// else {
+//      console.log('no puede entar ');
+// }
