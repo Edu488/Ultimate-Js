717f35e1c826d011f0d3fcc6b0d02323f7896ea8
Edu <edu488@gmail.com>
2024-04-03 09:41:22 -0500

    .

--- 04-Ejercicios/03.js
+++ 04-Ejercicios/03.js
@@ -1,13 +1,12 @@
 //indice que el valor no sea menor q cero y q exita en el array
-.
 
 
 function getbyIdx(arr, idx){
-//     if (idx < 0  || arr.length <= idx){
+    if (idx < 0  || arr.length <= idx){
 
-//         return 'elemento no existe';
-//     }
-//     return arr[idx];
+        return 'elemento no existe';
+    }
+    return arr[idx];
 }
 
  let resultado =  getbyIdx ([1, 2], 2)
---
+++ 04-Ejercicios/04.js
@@ -0,0 +1,9 @@
+let i = 0 ;
+
+
+
+for (let i = 0; i < 10; i++ ){
+    if(i % 2){
+    console.log("Impar",i);
+    }
+}
